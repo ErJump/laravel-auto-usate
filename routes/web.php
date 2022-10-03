25bc9910b7777e0df71504c42abb99d384bae930
pyroYV <simonerizzo.pvt@gmail.com>
2022-10-03 16:34:07 +0200

    Hardly working vue install

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'CarController@index');
 Route::resource('cars', 'CarController');
 Route::resource('optionals', 'OptionalsController');
+Route::get('{any}', function () {
+    return view('guest.home');
+  })->where('any', '.*');
+
